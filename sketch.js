7ca02aa25051e9cf0e39842503dc84a6d158ee3e
Kassandra Fuentes <kassandraf32528@students.laalliance.org>
2017-11-17 16:58:54 +0000

    Finished my project.

---
+++ sketch.js
@@ -0,0 +1,101 @@
+function setup() {
+  createCanvas(900,1000)
+}
+
+function draw() {
+  var x = 50
+
+  background('gray')
+  fill(250)
+ 
+ var x = 50;
+ 
+ 
+  var counter = 0
+  while (counter < 8){
+    if (counter % 2 === 0){
+      fill('white')
+    } else {
+      fill('black')
+    }
+
+    rect(40+(counter*40), 200, 40,40)
+    counter += 1 //counter += 1 
+ }
+ var counter = 0
+  while (counter < 8) {
+    if (counter % 2 === 0){
+      fill('black')
+    } else {
+      fill('white')
+    }
+
+    rect(40+(counter*40), 240, 40,40)
+    counter += 1 //counter += 1 
+  }
+  var counter = 0
+  while (counter < 8) {
+    if (counter % 2 === 0){
+      fill('white')
+    } else {
+      fill('black')
+    }
+
+    rect(40+(counter*40), 280, 40,40)
+    counter += 1 //counter += 1 
+}
+var counter = 0
+  while (counter < 8) {
+    if (counter % 2 === 0){
+      fill('black')
+    } else {
+      fill('white')
+    }
+
+    rect(40+(counter*40), 320, 40,40)
+    counter += 1 //counter += 1 
+}
+var counter = 0
+  while (counter < 8) {
+    if (counter % 2 === 0){
+      fill('white')
+    } else {
+      fill('black')
+    }
+
+    rect(40+(counter*40), 360, 40,40)
+    counter += 1 //counter += 1 
+}var counter = 0
+  while (counter < 8) {
+    if (counter % 2 === 0){
+      fill('black')
+    } else {
+      fill('white')
+    }
+
+    rect(40+(counter*40), 400, 40,40)
+    counter += 1 //counter += 1 
+}
+var counter = 0
+  while (counter < 8) {
+    if (counter % 2 === 0){
+      fill('white')
+    } else {
+      fill('black')
+    }
+
+    rect(40+(counter*40), 440, 40,40)
+    counter += 1 //counter += 1 
+}
+var counter = 0
+  while (counter < 8) {
+    if (counter % 2 === 0){
+      fill('black')
+    } else {
+      fill('white')
+    }
+
+    rect(40+(counter*40), 480, 40,40)
+    counter += 1 //counter += 1 
+}
+}
